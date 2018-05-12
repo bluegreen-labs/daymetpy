78c2637f276bb79ba15fe378835ec1ade7e7e8cd
Koen Hufkens <koen.hufkens@gmail.com>
2018-05-12 14:34:38 +0200

    pip config

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
   description = 'A library for accessing Daymet surface weather data',
   author = 'Koen Hufkens',
   author_email = 'koen.hufkens@gmail.com',
-  url = 'https://github.com/khufkens/daymetpy', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/1.0.0.tar.gz', # I'll explain this in a second
-  keywords = ['daymet', 'climatology', 'ORNL','weather'], # arbitrary keywords
+  url = 'https://github.com/khufkens/daymetpy',
+  download_url = 'https://github.com/peterldowns/mypackage/archive/1.0.0.tar.gz',
+  keywords = ['daymet', 'climatology', 'ORNL','weather'],
   classifiers = [],
 )
